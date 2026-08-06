6ae56e1bee4656b19d5cdaa37dacb0de1313760a
vmadhavan2310 <vinodmad34@gmail.com>
2026-08-06 17:05:26 +0530

    test

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -26,7 +26,7 @@ class CartDrawer extends HTMLElement {
     if (triggeredBy) this.setActiveElement(triggeredBy);
     const cartDrawerNote = this.querySelector('[id^="Details-"] summary');
     if (cartDrawerNote && !cartDrawerNote.hasAttribute('role')) this.setSummaryAccessibility(cartDrawerNote);
-    // here the animation doesn't seem to always get triggered. A timeout seem vinoth
+    // here the animation doesn't seem to always get triggered. A timeout seem QA test
     setTimeout(() => {this.classList.add('animate', 'active')});
 
     this.addEventListener('transitionend', () => {
