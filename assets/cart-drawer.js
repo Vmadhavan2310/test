77d106e5503fda11f146340c0afaa9154ed4f367
vmadhavan2310 <vinodmad34@gmail.com>
2026-08-06 19:51:56 +0530

    lint chek

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -22,6 +22,7 @@ class CartDrawer extends HTMLElement {
       }
     });
   }
+  console.log(kljkjkl)
   open(triggeredBy) {
     if (triggeredBy) this.setActiveElement(triggeredBy);
     const cartDrawerNote = this.querySelector('[id^="Details-"] summary');
